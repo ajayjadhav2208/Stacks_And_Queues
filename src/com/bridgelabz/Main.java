c9826ac25a0c7f4a6a74cf883e090b2c00b5e19a
ajayjadhav2208 <ajayjadhav22081997@gmail.com>
2021-10-03 19:54:16 +0530

    First UC Case

--- src/com/bridgelabz/Main.java
+++ src/com/bridgelabz/Main.java
@@ -1,9 +1,15 @@
 package com.bridgelabz;
 
 public class Main {
-
     public static void main(String[] args) {
-        System.out.println("Welcome to the Stack and Queues");
+        Stacks stack = new Stacks();
+        stack.push(70);
+        stack.push(30);
+        stack.push(56);
+        stack.display();
 
     }
 }
+
+
+
---
+++ src/com/bridgelabz/Stacks.java
@@ -0,0 +1,47 @@
+package com.bridgelabz;
+
+public class Stacks {
+    Node top;
+
+    class Node {
+        int data;
+        Node next;
+
+        public Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    public boolean isEmpty() {
+        if (top == null) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    //****************************UC1 Create a stacks of 56->30->70**************************
+    public void push(int data) {
+        Node newNode = new Node(data);
+        if (top == null) {
+            top = newNode;
+        } else {
+            Node temp = top;
+            top = newNode;
+            newNode.next = temp;
+        }
+        System.out.println("Element Push: " + top.data);
+    }
+
+    public void display() {
+        Node temp = top;
+        while (temp != null) {
+            System.out.print(temp.data + "->");
+            temp = temp.next;
+        }
+    }
+
+
+}
+
