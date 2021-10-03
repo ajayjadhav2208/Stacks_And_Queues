3625dc1b54fa3c465d18dc760350018843fb3e89
ajayjadhav2208 <ajayjadhav22081997@gmail.com>
2021-10-03 19:58:54 +0530

    Second UC Case

--- src/com/bridgelabz/Main.java
+++ src/com/bridgelabz/Main.java
@@ -6,8 +6,15 @@ public class Main {
         stack.push(70);
         stack.push(30);
         stack.push(56);
+
         stack.display();
 
+        System.out.println(stack.peak());
+
+        System.out.println(stack.pop());
+        System.out.println(stack.pop());
+        System.out.println(stack.pop());
+        stack.display();
     }
 }
 
